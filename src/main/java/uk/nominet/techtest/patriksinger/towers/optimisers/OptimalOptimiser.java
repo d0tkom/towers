4b25bfc96166d02bf6d8f018a1d6ca60456ff7e3
Patrik Singer <singerpatrik@gmail.com>
2018-05-27 21:13:14 +0200

    change power to cover closest receiver instead of just increasing by one

--- src/main/java/uk/nominet/techtest/patriksinger/towers/optimisers/OptimalOptimiser.java
+++ src/main/java/uk/nominet/techtest/patriksinger/towers/optimisers/OptimalOptimiser.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import uk.nominet.techtest.patriksinger.towers.calculators.ChebyshevDistanceCalculator;
+import uk.nominet.techtest.patriksinger.towers.calculators.DistanceCalculator;
 import uk.nominet.techtest.patriksinger.towers.calculators.SignalCalculator;
 import uk.nominet.techtest.patriksinger.towers.model.Boost;
 import uk.nominet.techtest.patriksinger.towers.model.Receiver;
@@ -13,7 +14,8 @@ import uk.nominet.techtest.patriksinger.towers.model.Scenario;
 import uk.nominet.techtest.patriksinger.towers.model.Transmitter;
 
 public class OptimalOptimiser implements PowerOptimiser {
-	private final SignalCalculator signalCalculator = new SignalCalculator(new ChebyshevDistanceCalculator());
+	private final DistanceCalculator distanceCalculator = new ChebyshevDistanceCalculator();
+	private final SignalCalculator signalCalculator = new SignalCalculator(distanceCalculator);
 	
 	public OptimalOptimiser() {
 	}
@@ -36,19 +38,16 @@ public class OptimalOptimiser implements PowerOptimiser {
 		List<Boost> boosts = new ArrayList<Boost>();
 
 		for (Transmitter t : transmitters) {
-			int boost = 0;
-			boolean keepGoing = true;
-			long currentCoverage = receivers.stream().filter(r -> signalCalculator.hasSignal(r, transmitters)).count();
-			while(keepGoing) {
-				boost++;
-				final Transmitter newTransmitter = new Transmitter(t.id, t.location, t.power + boost);
-				final List<Transmitter> newTransmitters = transmitters.stream().map(x -> x.id == t.id ? newTransmitter : new Transmitter(x.id, x.location, x.power)).collect(Collectors.toList());
-				long newCoverage = receivers.stream().filter(r -> signalCalculator.hasSignal(r, newTransmitters)).count();
-				if (newCoverage != currentCoverage) {
-					currentCoverage = newCoverage;
-					boosts.add(new Boost(t, boost, (int)currentCoverage));
-				}
-				keepGoing = !receivers.stream().allMatch(r -> signalCalculator.hasSignal(r, newTransmitters));
+			List<Receiver> receiversWithoutSignal = receivers.stream().filter(r -> !signalCalculator.hasSignal(r, transmitters)).collect(Collectors.toList());
+		
+			while (receiversWithoutSignal.size() > 0) {
+				Receiver closestReceiver = receiversWithoutSignal.stream().min((r1, r2) -> Integer.compare(distanceCalculator.distance(t.location, r1.location), distanceCalculator.distance(t.location, r2.location))).get();
+				int boostNeeded = distanceCalculator.distance(t.location, closestReceiver.location) - t.power;
+				Transmitter boostedTransmitter = new Transmitter(t.id, t.location, t.power + boostNeeded);
+				final List<Transmitter> transmittersWithBoostedTransmitter = transmitters.stream().map(x -> x.id == t.id ? boostedTransmitter : new Transmitter(x.id, x.location, x.power)).collect(Collectors.toList());
+				int coverageAfterBoost = (int)receivers.stream().filter(r -> signalCalculator.hasSignal(r, transmittersWithBoostedTransmitter)).count();
+				boosts.add(new Boost(t, boostNeeded, coverageAfterBoost));
+				receiversWithoutSignal = receivers.stream().filter(r -> !signalCalculator.hasSignal(r, transmittersWithBoostedTransmitter)).collect(Collectors.toList());
 			}
 		}
 		
