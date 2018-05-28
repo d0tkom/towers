fc8bef6fc7cac5248d315293d37388753c32e9ac
Patrik Singer <singerpatrik@gmail.com>
2018-05-28 22:54:57 +0200

    format

--- src/main/java/uk/nominet/techtest/patriksinger/towers/calculators/ChebyshevDistanceCalculator.java
+++ src/main/java/uk/nominet/techtest/patriksinger/towers/calculators/ChebyshevDistanceCalculator.java
@@ -9,9 +9,9 @@ public class ChebyshevDistanceCalculator implements DistanceCalculator {
 	
 	// Calculates the Chebyshev distance of two points
 	@Override
-    public int distance(Point p1, Point p2) {
-    	int xDiff = java.lang.Math.abs(p1.x - p2.x);
-    	int yDiff = java.lang.Math.abs(p1.y - p2.y);
-    	return java.lang.Math.max(xDiff, yDiff);
-    }	
+	public int distance(Point p1, Point p2) {
+		int xDiff = java.lang.Math.abs(p1.x - p2.x);
+		int yDiff = java.lang.Math.abs(p1.y - p2.y);
+		return java.lang.Math.max(xDiff, yDiff);
+	}	
 }
